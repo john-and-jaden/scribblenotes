fe4c5566bcea117cb9b1dd35ef81b612121baebf
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-08-28 17:34:36 -0400

    Add basic canvas with doodling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
 
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    let x = 0;
+    let y = 0;
+
+    let isMouseDown = false;
+    const paintCanvas = document.querySelector('.js-paint');
+    const context = paintCanvas.getContext('2d');
+    context.lineCap = 'round';
+
+    const stopDrawing = () => {
+      isMouseDown = false;
+    };
+    const startDrawing = (event) => {
+      isMouseDown = true;
+      [x, y] = [event.offsetX, event.offsetY];
+    };
+    const drawLine = (event) => {
+      if (isMouseDown) {
+        const newX = event.offsetX;
+        const newY = event.offsetY;
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(newX, newY);
+        context.stroke();
+        x = newX;
+        y = newY;
+
+        console.log(x);
+        console.log(y);
+      }
+    };
+
+    paintCanvas.addEventListener('mousedown', startDrawing);
+    paintCanvas.addEventListener('mousemove', drawLine);
+    paintCanvas.addEventListener('mouseup', stopDrawing);
+    paintCanvas.addEventListener('mouseout', stopDrawing);
+  }, []);
+
   return (
     <div className="App">
-      <canvas id="stockGraph" width="500" height="500" style={{ backgroundColor: 'green' }}>
-            Fallback
-      </canvas>
+      <canvas className="js-paint paint-canvas" width="600" height="300"></canvas>
     </div>
   );
 }
