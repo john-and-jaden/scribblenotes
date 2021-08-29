160228d02b344d8398fc7eecc005099d37b94972
JadenBalogh <jadenbalogh@gmail.com>
2021-08-28 22:54:24 -0400

    Add animated bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
+import * as Tone from 'tone';
+import anime from 'animejs';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 
 function App() {
+  var [barProgress, setBarProgress] = useState(0);
+
+  let barX = 0;
+
+  function musicTest() {
+    // console.log(synth);
+    // synth.triggerAttackRelease('C4', '8n');
+  }
+
+  const musicInterval = setInterval(() => {
+    barX++;
+    if (barX > 800) {
+      barX = 0;
+    }
+
+    // SEE: tonejs (https://tonejs.github.io/)
+    // todo: have a dictionary of Synth objects for each note,
+    // manage each object's attack/release based on whether we currently
+    // have a point within the y-range of that note under the bar
+    // let idx = barX % notes.length;
+    // let synth = new Tone.Synth().toDestination();
+    // synth.triggerAttackRelease(notes[idx], '4n');
+  }, 10);
+
   useEffect(() => {
     let x = 0;
     let y = 0;
@@ -35,14 +63,38 @@ function App() {
     };
 
     paintCanvas.addEventListener('mousedown', startDrawing);
+    paintCanvas.addEventListener('mousedown', musicTest);
     paintCanvas.addEventListener('mousemove', drawLine);
     paintCanvas.addEventListener('mouseup', stopDrawing);
     paintCanvas.addEventListener('mouseout', stopDrawing);
+
+    let timeline = anime
+      .timeline({
+        update: ({ progress }) => {
+          setBarProgress(progress);
+          console.log(progress);
+        },
+        loop: true
+      })
+      .add({
+        duration: 4000,
+      });
   }, []);
 
   return (
-    <div className="App">
-      <canvas className="js-paint paint-canvas" width="800" height="400"></canvas>
+    <div className='App'>
+      <input
+        id='progress-bar'
+        type='range'
+        min='0'
+        max='100'
+        value={barProgress}
+      />
+      <canvas
+        className='js-paint paint-canvas'
+        width='800'
+        height='400'
+      ></canvas>
     </div>
   );
 }
